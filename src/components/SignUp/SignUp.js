ad9602530468121fdce992a0947c32b01c1be0b6
Đăng Thiện <71754331+dangthien0973@users.noreply.github.com>
2022-06-05 14:21:05 +0700

    up

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -322,6 +322,8 @@ function SignUp(props) {
          postData('http://localhost:8011/api/auth/signup', { username: values.nickname, password:"123", email: "dangthien0973@gmail.com", customername: "nguyeenx dang Thien", })
   .then(data => {
     console.log(data); // JSON data parsed by `data.json()` call
+    alert(data.message)
+    
   });
         }
         return (
@@ -339,19 +341,17 @@ async function postData(url = '', data = {}) {
   // Default options are marked with *
   const response = await fetch(url, {
     method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    // mode: 'no-cors', // no-cors, *cors, same-origin
+   //  mode: 'no-cors', // no-cors, *cors, same-origin
     // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     credentials: 'same-origin', // include, *same-origin, omit
     headers: {
       'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
     },
     redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
+    referrerPolicy: 'no-referrer', 
+    body: JSON.stringify(data) 
   });
-  console.log(response);
-  return response.json(); // parses JSON response into native JavaScript objects
+  return response.json(); 
 }
 
 SignUp.propTypes = {}
