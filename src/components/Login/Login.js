54cd9759bcf3c311ac2d351b9930dbcd27516b40
Vik <dvtd2000@gmail.com>
2022-06-08 16:52:39 +0700

    Demo validate onBlur

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,94 +1,127 @@
-import React, { useState } from 'react';
-
-
+import React, { useState } from "react";
 
 //Adding antd modules and style
-import { Button, Modal, Form, Input, Radio } from 'antd';
+import { Button, Modal, Form, Input, Radio } from "antd";
 // import "antd/dist/antd.css";
-import { QuestionCircleOutlined } from '@ant-design/icons';
+import { QuestionCircleOutlined } from "@ant-design/icons";
 
 function Login(props) {
-  
-    const CollectionCreateForm2 = ({ visible, onCreate, onCancel }) => {
-      const [form] = Form.useForm();
-      return (
-        <Modal
-          visible={visible}
-          title="Login"
-          okText="Login"
-          cancelText="Cancel"
-          onCancel={onCancel}
-          onOk={() => {
-            form
-              .validateFields()
-              .then((values) => {
-                form.resetFields();
-                onCreate(values);
-              })
-              .catch((info) => {
-                console.log('Validate Failed:', info);
-              });
+  const CollectionCreateForm2 = ({ visible, onCreate, onCancel }) => {
+    const [form] = Form.useForm();
+    const handleValidateEmail = async (e) => {
+      const emailValue = e.target.value;
+      console.log("email value", emailValue);
+      try {
+        const request = {
+          email: emailValue,
+        };
+
+        //Call API validate email
+        const validateResponse = { status: "failed" };
+        if (validateResponse?.status === "success") {
+          //Do something
+        } else {
+          console.log("validate failed");
+          form.setFields([
+            {
+              name: "email",
+              errors: ["Email is already exist"],
+            },
+          ]);
+        }
+      } catch (error) {}
+    };
+    return (
+      <Modal
+        visible={visible}
+        title="Login"
+        okText="Login"
+        cancelText="Cancel"
+        onCancel={onCancel}
+        onOk={() => {
+          form
+            .validateFields()
+            .then((values) => {
+              form.resetFields();
+              onCreate(values);
+            })
+            .catch((info) => {
+              console.log("Validate Failed:", info);
+            });
+        }}
+      >
+        <Form
+          form={form}
+          layout="vertical"
+          name="form_in_modal"
+          initialValues={{
+            modifier: "public",
           }}
         >
-          <Form
-            form={form}
-            layout="vertical"
-            name="form_in_modal"
-            initialValues={{
-              modifier: 'public',
-            }}
+          <Form.Item
+            name="email"
+            label="E-mail"
+            rules={[
+              {
+                type: "email",
+                message: "The input is not valid E-mail!",
+              },
+              {
+                required: true,
+                message: "Please input your E-mail!",
+              },
+            ]}
+            onBlur={handleValidateEmail}
           >
-            <Form.Item
-                name="email"
-                label="E-mail"
-                rules={[
-                  {
-                    type: 'email',
-                    message: 'The input is not valid E-mail!',
-                  },
-                  {
-                    required: true,
-                    message: 'Please input your E-mail!',
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
-            <Form.Item name="password" label="Password"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please enter password!',
-                },
-              ]}
-            >
-              <Input type="password" />
-            </Form.Item>
-  
-          </Form>
-        </Modal>
-      );
-  
-    };
-     const CollectionsPage2 = () => {
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[
+              {
+                required: true,
+                message: "Please enter password!",
+              },
+            ]}
+          >
+            <Input type="password" />
+          </Form.Item>
+        </Form>
+      </Modal>
+    );
+  };
+  const CollectionsPage2 = () => {
     const [visible, setVisible] = useState(false);
 
     const onCreate = (values) => {
-      console.log('Received values of form: ', values);
+      console.log("Received values of form: ", values);
       setVisible(false);
-    }
+    };
     return (
       <div className="header-info-right">
-        <Button type="primary" onClick={() => { setVisible(true) }}> Login </Button>
-        <CollectionCreateForm2 visible={visible} onCreate={onCreate} onCancel={() => { setVisible(false); }} />
+        <Button
+          type="primary"
+          onClick={() => {
+            setVisible(true);
+          }}
+        >
+          {" "}
+          Login{" "}
+        </Button>
+        <CollectionCreateForm2
+          visible={visible}
+          onCreate={onCreate}
+          onCancel={() => {
+            setVisible(false);
+          }}
+        />
       </div>
     );
-  }
-  return (
-      <CollectionsPage2 />
-  );
+  };
+  return <CollectionsPage2 />;
 }
 
-Login.propTypes = {}
+Login.propTypes = {};
 
-export default Login
+export default Login;
